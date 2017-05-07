ee1392f6fc744543ccf3b5df1a2159b188dc5c7b
Ivan Vladimir Meza Ruiz <ivanvladimir@gmail.com>
2017-05-06 23:03:49 -0500

    Scripts to extract links

---
+++ scripts/extract_links.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function check {
+	ANS='yes'
+	echo $1
+	if [ -f $1 ]; then
+		read -r -p "${1} exists, $2? [y/N] " response
+		if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+		then
+			ANS='yes'
+		else
+			ANS='no'
+		fi
+	fi
+}
+
+
+function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
+
+function extract {
+	LANGS=$*
+	for LANG in $LANGS
+	do
+		echo "Procesing $LANG ..."
+		ARGS=()
+		for LANG2 in $LANGS
+		do
+			if [ $LANG != $LANG2 ] 
+			then
+				ARGS+=($LANG2)
+			fi
+		done
+		EXP=`join_by '\|' ${ARGS[@]}`
+		echo "Looking for $EXP ..."
+		python python/mysqldump_to_csv.py data/wiki${LANG}.links.sql | grep ",\(${EXP}\)," > data/${LANG}wiki.links.csv
+	done
+}
+
+
+mkdir -p data
+extract tk es en pl pt de cy fa fi fr he  it ru  el ar 
+#extract it fi pl
